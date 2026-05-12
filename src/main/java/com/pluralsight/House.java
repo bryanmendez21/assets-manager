983092b9853329ce8c1cd192668a30d0fa2b1130
bryan <bryanmendez21@icloud.com>
2026-05-12 11:00:52 -0400

    - create house and vehicle class - add attributes, constructor, getter and setters to house class - override getValue from asset class in house class

---
+++ src/main/java/com/pluralsight/House.java
@@ -0,0 +1,73 @@
+package com.pluralsight;
+
+public class House extends Asset{
+
+    protected String address;
+    protected int condition;
+    protected int squareFoot;
+    protected int lotSize;
+
+    public House(String description, String dateAcquired, double originalCost, String address, int condition, int squareFoot, int lotSize) {
+        super(description, dateAcquired, originalCost);
+        this.address = address;
+        this.condition = condition;
+        this.squareFoot = squareFoot;
+        this.lotSize = lotSize;
+    }
+
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public int getCondition() {
+        return condition;
+    }
+
+    public void setCondition(int condition) {
+        this.condition = condition;
+    }
+
+    public int getSquareFoot() {
+        return squareFoot;
+    }
+
+    public void setSquareFoot(int squareFoot) {
+        this.squareFoot = squareFoot;
+    }
+
+    public int getLotSize() {
+        return lotSize;
+    }
+
+    public void setLotSize(int lotSize) {
+        this.lotSize = lotSize;
+    }
+
+    @Override
+    public double getValue(){
+         int x = 1;
+         double value = ((x * squareFoot) + (.25 * squareFoot));
+
+        if (condition == 1){
+            x = 180;
+            return value;
+        } else if(condition == 2){
+            x = 130;
+            return value;
+        } else if (condition == 3){
+            x = 90;
+            return value;
+        } else if (condition == 4){
+            x = 80;
+            return value;
+        } else {
+            return 0;
+        }
+    }
+
+}
